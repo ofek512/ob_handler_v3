550ee448d9daa05e7b80d0ac72f3a535a4c0d15c
ofek512 <95701220+ofek512@users.noreply.github.com>
2021-12-13 16:16:14 +0200

    Processor syntax and comments update

--- ob_handler_v3/processor.py
+++ ob_handler_v3/processor.py
@@ -47,25 +47,25 @@ class Worker:
 def GetTask():
     
     for i in range(params.data_availability_check_timeout):
-        #creating a list that contains small lists that in each list, all the L2s have the same L3 "target"
+        # creating a list that contains small lists that in each list, all the L2s have the same L3 "target"
         checker =  True
-        initial_list = sql.GetFilesReadyForProcessing()#initial list that contains all the tuples of L2s in the database
-        fatherList = [] #the list that contains all the lists
-        littlemanlist = [] #the list inside fatherlist that all the tuples have the same L3
-        targetCheck = initial_list[0][2]#the first L3 "target" before the for
-        for man in initial_list #this for sorts all the lists into same L3 and stores these lists into the fatherList
-            if targetCheck == man[2]:#if the tuple has the same L3 as the target check
-                littlemanlist.append(man)
+        initial_list = sql.GetFilesReadyForProcessing()# initial list that contains all the tuples of L2s in the database
+        father_list = [] # the list that contains all the lists
+        inner_list = [] # the list inside fatherlist that all the tuples have the same L3
+        target_check = initial_list[0][2] # the first L3 "target" before the for
+        for list_info in initial_list # this for sorts all the lists into same L3 and stores these lists into the fatherList
+            if target_check == list_info[2]: # if the tuple has the same L3 as the target check 
+                inner_list.append(list_info)
             else:
-                targetCheck = man[2]
-                fatherList.append(littlemanlist[:])#inserting the little list into the father list
-                littlemanlist = []#initialising the litte list
-        for littlemanlist in fatherList #checking for the first little list that all of the L2s inside are downloaded.
-            for alldashid in littlemanlist:
-                if alldashid[1] != 1:
+                target_check = list_info[2]
+                father_list.append(inner_list[:]) # inserting the little list into the father list we used [:] so it wont be a reference but the obj itself!
+                inner_list = [] # initialising the litte list
+        for inner_list in father_list  # checking for the first little list that all of the L2s inside are downloaded.
+            for inner_list_info in inner_list:
+                if inner_list_info[1] != 1: # [1] is the file status. 0 not downloaded 1 downloaded 2 processed
                     checker = False
-            if checker: #if checker is true, it means all the L2 in the list are downloaded and its ready to get processed.
-                return [item[0] for item in littlemanlist]
+            if checker:  # if checker is true, it means all the L2 in the list are downloaded and its ready to get processed.
+                return [item[0] for item in inner_list] #returning a list of all the 
         print ("No list was found suited for processing... waiting 60 minutes.")
      
 
