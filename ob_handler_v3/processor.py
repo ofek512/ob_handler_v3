ef3087d5bfd354450cf1356d019b1855d97a3bec
Lun Surdyaev <lsurdyae@campus.haifa.ac.il>
2021-12-14 20:38:40 +0200

    Fixed another one

--- ob_handler_v3/processor.py
+++ ob_handler_v3/processor.py
@@ -40,7 +40,7 @@ class Worker(Thread): # a class of a worker, a sinle thread in our glorious mult
             try:
                 task = self.queue.get(block=True)
                 print("Worker", self.id, "given a task.")
-                self.Execute(','.join(task)) # executing said task
+                self.Execute(task) # executing said task
             except Exception as e:
                 print(datetime.now(), "Worker", self.id, "threw an exception:", e) 
             finally:
@@ -189,7 +189,7 @@ def main():
 
         task = GetTask()
 
-        tasks.put(task)
+        tasks.put(','.join(task))
 
     tasks.join()
 
