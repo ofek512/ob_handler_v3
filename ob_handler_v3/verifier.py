09de86d1a4fbae14bed10394b3af5c84065b4d13
ofek512 <95701220+ofek512@users.noreply.github.com>
2022-01-03 18:18:59 +0200

    Started working on L2 verifier
    Need to add a checker if the target L3 is processed or not

--- ob_handler_v3/verifier.py
+++ ob_handler_v3/verifier.py
@@ -1,7 +1,7 @@
 """
 File Management Database Verifier Script
 Created by Lun Surdyaev on 2021-12-03
-Last Updated on 2022-01-02
+Last Updated on 2022-01-03
 Maintained by Ofek Yankis ofek5202@gmail.com
 
 Description:
@@ -22,7 +22,46 @@ import _util as util
 import _sqlhandler as sql
 
 def HandleL2():
-    pass
+    print("Getting list of all L3m files on the disk...", end=' ', flush=True)
+    l2List = util.GetExistingFilenamesAndPaths(params.path_to_data+"L2/") # gets a list that [0] is the location and [1] is the filename
+    print("Done.")
+
+    print("Verifying those files' entries in the database...", flush=True)
+    location_pointer = 0
+    for filename in l2List[1]:
+
+        # get file status
+        status = sql.GetFileStatus("L2_files", filename)
+       
+        # if the file isn't in the DB at all, notify user and insert it
+        if status is None:
+            print("The file", filename, "was not found in the database. Inserting it.")
+            entry = {"id": filename, "location": l2List[0][location_pointer], "file_status": 1}
+            sql.InsertL3m(entry)
+
+        # else, check if the database displays correct status
+        elif status == 0:
+            print("The file", filename, "was listed as missing despite its presence on the disk. Marking it as existing.")
+            sql.UpdateStatus("L3m_files", filename, 1)
+
+        # as this file was dealt with, set its verifier bit to 1
+        sql.VerifyFile("L2_files", filename)
+        location_pointer += 1
+    print("Existing file verification done.")
+
+    # if there are any database entries remaining that are listed as existing,
+    # but aren't on the disk, notify user and update DB
+    print("Verifying that no missing files were marked as existing...")
+    unverified_existing = sql.GetUnverifiedExisting("L3m_files") # need to add the function
+    for id in unverified_existing:
+        print("The file", id, "is listed as existing on the DB, but is not present on the disk. Marking it as missing.")
+        sql.UpdateStatus("L2_files", id, 0)
+
+    # reset the verifier bit to 0
+    sql.ResetVerifier("L2_files")
+
+    print("L2 Verification done.")
+
 
 def HandleL3m():
 
