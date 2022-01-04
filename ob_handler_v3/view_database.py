55d0f53596a465b4d5dc7e2598a072666beefb70
ofek512 <95701220+ofek512@users.noreply.github.com>
2022-01-04 16:11:54 +0200

    Update view_database.py

--- ob_handler_v3/view_database.py
+++ ob_handler_v3/view_database.py
@@ -2,20 +2,38 @@ import params
 import _util as util
 import _sqlhandler as sql
 
+def UserInput():
+    try:
+        start_date = datetime.strptime(
+            input("Please put the start date from which you want the data, in the following format: YYYY-MM-DD.\nYour answer: "),
+            "%Y-%m-%d")
+        end_date   = datetime.strptime(
+            input("Please put the last date from which you want the data, in the following format: YYYY-MM-DD.\nYour answer: "),
+            "%Y-%m-%d")       
+    except:
+        print("An invalid date was entered.")
+        exit("Program terminated.")
 
-
-
-
-
-
-
-
-
+    return start_date, end_date, 
 
 
 def main():
     
-    print("Write the files' dates which you want to view the database in the form of ")
+    start_date, end_date = UserInput()  
+    files = sql.GetExisting("L3m_files") 
+    
+    for f in files:
+        properties = util.GetFileProperties(file)
+        if properties["date"] >= start_date and properties["date"] <= end_date:
+            # the logic is in here
+    
+    
+    
+
+    
+    
+    
+    
     
     
     
