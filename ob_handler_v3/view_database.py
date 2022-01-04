322dae893c4aaec744d677c4fce8b974b651d79e
ofek512 <95701220+ofek512@users.noreply.github.com>
2022-01-04 15:48:26 +0200

    Update view_database.py

--- ob_handler_v3/view_database.py
+++ ob_handler_v3/view_database.py
@@ -14,6 +14,10 @@ import _sqlhandler as sql
 
 
 def main():
-
+    
+    print("Write the files' dates which you want to view the database in the form of ")
+    
+    
+    
 if __name__ == "__main__":
     main()
