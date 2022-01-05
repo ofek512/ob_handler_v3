4414ae2b2ce05f2cb8085a38a33e39bde2657c94
ofek512 <95701220+ofek512@users.noreply.github.com>
2022-01-05 14:09:13 +0200

    Finished
    yeeeee man

---
+++ ob_handler_v3/change_status.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+
+import params
+import _util as util
+import _sqlhandler as sql
+
+
+def UserInput():
+    try:
+        start_date = datetime.strptime(
+            input("Please put the start date from which you want the data, in the following format: YYYY-MM-DD.\nYour answer: "),
+            "%Y-%m-%d")
+        end_date   = datetime.strptime(
+            input("Please put the last date from which you want the data, in the following format: YYYY-MM-DD.\nYour answer: "),
+            "%Y-%m-%d")       
+    except:
+        print("An invalid date was entered.")
+        exit("Program terminated.")
+
+    return start_date, end_date, 
+
+
+def main():
+    
+    start_date, end_date = UserInput()  
+    files = sql.GetExisting("L3m_files") 
+    for f in files:
+        properties = util.GetFileProperties(f)
+        if properties["date"] >= start_date and properties["date"] <= end_date:
+            sql.UpdateStatus("L3m_files" ,f ,1)
+            print("The file name's: ", f, "status has changed to: ", sql.GetFileStatus("L3m_files", f))
+
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
