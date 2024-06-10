cdbdce0f1b73c342a00f107290df9b2c403cc656
Ofek <ofek5202@gmail.com>
2024-06-10 11:57:15 +0300

    i added a test OOGA BOOGA to see if git works on my IDE

--- ob_handler_v3/_sqlhandler.py
+++ ob_handler_v3/_sqlhandler.py
@@ -2,7 +2,7 @@
 SQL Handling Utility
 Created by Lun Surdyaev on 2021-12-04
 Last Updated on 2022-01-03
-Maintained by Ofek Yankis ofek5202@gmail.com
+Maintained by Ofek Yankis ofek5202@gmail.com OOGA BOOGA
 Description:
 This is a utility for handling the SQL-based File Management Database.
 It uses the constants defined in params.py to find the database, and provides an interface to it via its functions.
@@ -62,7 +62,7 @@ select_ready_for_download = """ SELECT id, download_url
 select_ready_for_processing = """ SELECT id, file_status, target
                                     FROM L2_files
                                     ORDER BY priority ASC, target ASC
-                                    """ 
+                                    """
 select_unverified_existing = """ SELECT id
                                     FROM {0}
                                     WHERE verifier_bit=0
@@ -115,10 +115,10 @@ reset_verifier = """UPDATE {0}
                         SET verifier_bit=0"""
 # deleting files
 delete_L2file = """ DELETE FROM L2_files WHERE id='{0}'"""
-                       
+
 
 # execute a given query, and return a value according to the return_type argument
-def Execute(query, return_type = None):
+def Execute(query, return_type=None):
     try:
         # preparation
         conn = sqlite3.connect(params.path_to_data + params.db_filename, isolation_level=None)
@@ -130,7 +130,7 @@ def Execute(query, return_type = None):
         try:
             # execute all commands
             for com in commands:
-                #print("Executing", com)
+                # print("Executing", com)
                 cur.execute(com)
 
             # decide on return value
@@ -156,13 +156,16 @@ def Execute(query, return_type = None):
 
     return retval
 
+
 # checks if an entry exists in the specified table
 def Exists(table, entry):
     return bool(Execute(count_files.format(table, entry), "scalar"))
 
+
 # checks if an entry exists in a table with status > 0
 def ExistsOnDisk(table, entry):
     return bool(Execute(count_existing.format(table, entry), "scalar"))
+
 
 # converts a filename into a dictionary containing all fields
 def FilenameToDict(filename, location):
@@ -173,29 +176,33 @@ def FilenameToDict(filename, location):
     p = util.GetFileProperties(filename)
     if p["level"] == "L2":
         d["target"] = util.ProduceL3mFilename(filename)
-        
+
     return d
+
 
 # formats an entry (given in the form of a dictionary) into a fields-values pair
 def FormatEntry(entry):
     formatted_entry = dict()
     for item in entry.items():
         if isinstance(item[1], str):
-            formatted_entry[item[0]] = "'"+item[1]+"'"
+            formatted_entry[item[0]] = "'" + item[1] + "'"
         elif isinstance(item[1], int):
             formatted_entry[item[0]] = str(item[1])
 
     return ','.join(formatted_entry.keys()), ','.join(formatted_entry.values())
-  
+
+
 # updates L3m file location and status
 def UpdateL3m(location, filename):
     Execute(update_L3m.format(location, filename))
+
 
 # insert a L3m file into the database
 def InsertL3m(entry):
     query = insert_L3m.format(*FormatEntry(entry))
     Execute(query)
-    
+
+
 # insert an existing L2 file into the database
 def InsertL2(entry):
     # check if a target L3m entry exists
@@ -208,93 +215,111 @@ def InsertL2(entry):
     # otherwise, insert the L2 entry with file_status=1 ('unprocessed') and insert a L3m entry with file_status=0 ('missing')
     else:
         entry["file_status"] = 1
-        L3m_entry = {"id":entry["target"], "file_status":0}
+        L3m_entry = {"id": entry["target"], "file_status": 0}
         Execute(insert_L2_unprocessed.format(*FormatEntry(entry), *FormatEntry(L3m_entry)))
-      
+
+
 # insert files from a certain type and from a certain folder into the DB
 def InsertFiles(path, filetype):
     filelist = os.listdir(path)
     for item in filelist:
 
         # if the item is a directory, go through it recursively
-        if os.path.isdir(path+item):
-            InsertFiles(path+item+'/', filetype)
+        if os.path.isdir(path + item):
+            InsertFiles(path + item + '/', filetype)
 
         # else, try inserting the file
         else:
             db_entry = FilenameToDict(item, path)
 
             if filetype == "L2":
-                db_entry["priority"] = 4 # the default priority for L2 files that already exist
+                db_entry["priority"] = 4  # the default priority for L2 files that already exist
                 InsertL2(db_entry)
             else:
                 db_entry["file_status"] = 1
                 InsertL3m(db_entry)
 
+
 # get all existing files from a table
 def GetExisting(table):
     return [item[0] for item in Execute(select_existing.format(table), "list")]
+
 
 # get <limit> files that are ready to be downloaded
 def GetReadyForDownload(limit):
     return Execute(select_ready_for_download.format(limit), "list")
 
-# gets filestatus  
+
+# gets filestatus
 def GetFileStatus(table, filename):
     return Execute(get_file_status.format(table, filename))
-  
+
+
 # gets all the unverified files 
 def GetUnverifiedExisting(table):
     return Execute(select_unverified_existing.format(table))
-  
+
+
 # queue up a L2 file to be downloaded
 def QueueFile(entry):
     entry["file_status"] = 0
-    L3m_entry = {"id":entry["target"], "file_status":0}
+    L3m_entry = {"id": entry["target"], "file_status": 0}
     Execute(insert_L2_unprocessed.format(*FormatEntry(entry), *FormatEntry(L3m_entry)))
+
 
 # update the entry concerning the specified L2 file, when it has been downloaded
 def FileDownloaded(filename, location):
     Execute(file_downloaded.format(filename, location, datetime.now().strftime("%Y-%m-%d %H:%M")))
 
+
 # update the entry concerning the specified L3m file, when it has been produced
 def FileProduced(filename, location):
     Execute(file_produced.format(filename, location, datetime.now().strftime("%Y-%m-%d %H:%M")))
+
 
 # update the given file's status in the given table
 def UpdateStatus(table, filename, status):
     Execute(update_status.format(table, filename, status))
 
-# update the give file's priority in the given table    
+
+# update the give file's priority in the given table
 def UpdatePriority(table, filename, priority):
-    Execute(update_priority.format(table, filename, priority))    
-    
+    Execute(update_priority.format(table, filename, priority))
+
+
 # set verifier_bit to 1
 def VerifyFile(table, filename):
     Execute(verify_file.format(table, filename))
+
 
 # set all files's verifier_bit to 0
 def ResetVerifier(table):
     Execute(reset_verifier.format(table))
 
+
 # get all files that fullfill verifier_bit=0 AND file_status>0:
 def GetUnverifiedExisting(table):
     return [item[0] for item in Execute(select_unverified_existing.format(table), "list")]
+
 
 # get all files that are ready for processing
 def GetFilesReadyForProcessing():
     return Execute(select_ready_for_processing, "list")
 
+
 def GetFileLocation(table, filename):
     return Execute(get_file_location.format(table, filename), "scalar")
+
 
 # returns false if and only if the database doesn't contain any L2 files with status 1
 def ThereAreUnprocessedFiles():
     return bool(Execute(count_unprocessed_files, "scalar"))
-  
+
+
 def DeleteSpecificFile(location, filename):
-    os.remove(location ,filename, dir_fd=none)
+    os.remove(location, filename, dir_fd=none)
     return Execute(delete_L2file.format(filename))
+
 
 if __name__ == "__main__":
     # if run as its own script, this produces the File Management Database
@@ -305,8 +330,8 @@ if __name__ == "__main__":
     # cycle through all files in the data directory recursively and insert them into the DB (if they aren't there already)
     # this ordering is important, as inserting L2 files will check whether L3m files are in the database, and not on the disk itself.
     print("Inserting L3m files...", end=' ', flush=True)
-    InsertFiles(params.path_to_data+"L3m/", "L3m")
+    InsertFiles(params.path_to_data + "L3m/", "L3m")
     print("Done.")
     print("Inserting L2 files...", end=' ', flush=True)
-    InsertFiles(params.path_to_data+"L2/", "L2")
+    InsertFiles(params.path_to_data + "L2/", "L2")
     print("Done.")
